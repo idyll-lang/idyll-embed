3b974427c93dbe54803cbacf69f8b36e928954ff
Matthew Conlen <mc@mathisonian.com>
2018-08-27 16:48:10 -0700

    update for JSX components

--- webpack.config.js
+++ webpack.config.js
@@ -2,5 +2,21 @@ module.exports = {
   entry: './index.js',
   output: {
     filename: 'idyll-embed.min.js'
+  },
+  module: {
+    rules: [
+      // the 'transform-runtime' plugin tells babel to require the runtime
+      // instead of inlining it.
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
+    ]
   }
 };
